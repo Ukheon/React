144c8fff3e56c9e0c25f65d572c31156c892c922
Ukwon <dnrgjs33@naver.com>
2022-02-08 04:26:44 +0900

    delete react-masterclass name

--- react-masterclass/src/routes/Chart.tsx
+++
@@ -1,8 +0,0 @@
-import { useParams } from "react-router-dom";
-function Chart() {
-	const test = useParams();
-	console.log(test, "chart");
-	return <div>chart</div>;
-}
-
-export default Chart;
--- react-masterclass/src/routes/Price.tsx
+++
@@ -1,8 +0,0 @@
-import { useParams } from "react-router-dom";
-function Price() {
-	const test = useParams();
-	console.log(test, "price");
-	return <div>price</div>;
-}
-
-export default Price;
