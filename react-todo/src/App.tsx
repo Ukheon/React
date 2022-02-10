0f3c07e4bd0702335a166b47724591147a5b23ca
ukheon <dnrgjs33@naver.com>
2022-02-10 14:03:48 +0900

    go

--- react-todo/src/App.tsx
+++ react-todo/src/App.tsx
@@ -69,7 +69,6 @@ a {
 
 function App() {
     const isDark = useRecoilValue(isDarkAtom);
-    console.log("hi");
     return (
         <>
             <ThemeProvider theme={isDark ? lightTheme : darkTheme}>
